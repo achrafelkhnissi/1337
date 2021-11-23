2ecf935bd1ed4ddd05a1ba40a8a0649104185f49
Achraf El khnissi <ael-khni@e2r10p15.1337.ma>
2021-11-23 18:07:57 +0100

    done

--- 42curses/ft_printf/main.c
+++ 42curses/ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: ael-khni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:56:38 by ael-khni          #+#    #+#             */
-/*   Updated: 2021/11/23 18:03:20 by ael-khni         ###   ########.fr       */
+/*   Updated: 2021/11/23 18:07:40 by ael-khni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 int	main(void)
 {
-	int test = 100;
-	int	ret;
 	printf("ft_printf: \n");
 	ret = ft_printf("% d\n", -10);
 	ft_printf("ret  %d\n", ret);
@@ -26,4 +24,3 @@ int	main(void)
 	printf("ret  %x\n", ret);
 	return (0);
 }
-
